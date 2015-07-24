eb432e04393ebdbda4f5cf4b8406e81d600ed580
John Edmonds <john.a.edmonds@gmail.com>
2015-07-24 04:21:40 +0000

    Serve requests with RecipeServlet.

--- src/main/java/com/breadbreadbook/server/BreadBreadBookModule.java
+++ src/main/java/com/breadbreadbook/server/BreadBreadBookModule.java
@@ -4,10 +4,12 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.mongodb.MongoClient;
 import com.google.inject.servlet.ServletModule;
+import com.breadbreadbook.server.rest.RecipeServlet;
 
 public class BreadBreadBookModule extends ServletModule {
   @Override
   public void configureServlets() {
+    serve("/api/recipes/*").with(RecipeServlet.class);
   }
   
   @Provides
